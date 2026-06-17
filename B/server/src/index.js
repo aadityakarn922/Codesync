2f25d8bd035cb0b727250c8d235ffe1e2f98345a
Aaditya Karn <aadityakarn922@gmail.com>
2026-06-17 09:18:08 +0545

    Update index.js

--- B/server/src/index.js
+++ B/server/src/index.js
@@ -9,7 +9,14 @@ const Room = require("./models/room");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://your-vercel-app.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 connectDB();
